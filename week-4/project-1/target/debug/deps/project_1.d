7e96a3febd195aa543c598c207a991d259e6b799
n.igwe <148016050+NmasiIgwe@users.noreply.github.com>
2023-11-03 12:15:16 +0100

    Delete week-4/project-1 directory

--- week-4/project-1/target/debug/deps/project_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\User\Desktop\n.igweCOS101\week-4\project-1\target\debug\deps\project_1.exe: src\main.rs
-
-C:\Users\User\Desktop\n.igweCOS101\week-4\project-1\target\debug\deps\project_1.d: src\main.rs
-
-src\main.rs:
