c8d3399e8e5ee40772aae9012528eba6d63fa665
n.igweCOS101 <nmasi.igwe@pau.edu.ng>
2023-11-29 11:45:48 +0100

    (My sixth commit)

---
+++ week-7/practice-5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\User\OneDrive\Desktop\n.igweCOS101\week-7\practice-5\target\debug\deps\practice_5.exe: src\main.rs
+
+C:\Users\User\OneDrive\Desktop\n.igweCOS101\week-7\practice-5\target\debug\deps\practice_5.d: src\main.rs
+
+src\main.rs:
